c1b78f95e45bbcf50fecded62848cb13c72f6fd7
NolanFT <nolan.f.tandun@gmail.com>
2025-12-18 00:42:46 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 
-define('LARAVEL_START', microtime(true));
+try {
+    require __DIR__ . '/../vendor/autoload.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
+    $response->send();
+    $kernel->terminate($request, $response);
+} catch (Throwable $e) {
+    echo "<pre>";
+    echo "FATAL ERROR\n\n";
+    echo $e->getMessage() . "\n\n";
+    echo $e->getFile() . ":" . $e->getLine() . "\n\n";
+    echo $e->getTraceAsString();
+    echo "</pre>";
+    exit;
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
